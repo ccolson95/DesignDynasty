7653198df766fca735ebd72d628ad633f9162035
caro <carolineo@smu.edu>
2014-10-30 15:08:10 -0500

    Env and robot

---
+++ FirstYearDesign/src/Env.java
@@ -0,0 +1,34 @@
+//import javax.xml.bind.ValidationEvent;
+
+import rxtxrobot.RXTXRobot;
+import rxtxrobot.AnalogPin;
+import rxtxrobot.ArduinoNano;
+
+public class Env {
+
+		RXTXRobot r;
+		final private static int PING_PIN = 12; 
+		
+		public Env(){
+			r = new ArduinoNano(); // Create RXTXRobot object 
+			r.setPort("/dev/tty.wch ch341 USB=>RS232 1410"); 
+			r.connect(); 
+		}
+		
+		public void close(){
+			r.close(); 
+		}
+		
+
+		public void checkTurbidity(){
+			
+			
+		}
+		
+		
+		public void checkSalinity(){
+			
+			
+			
+		}
+}
