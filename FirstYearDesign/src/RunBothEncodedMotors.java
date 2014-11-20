be7af7b18e13bc20944c772bff94e87a4fe80cee
gabbyrajan <grajan@smu.edu>
2014-11-20 15:13:13 -0600

    hi

--- FirstYearDesign/src/RunBothEncodedMotors.java
+++ FirstYearDesign/src/RunBothEncodedMotors.java
@@ -5,15 +5,15 @@ public class RunBothEncodedMotors {
 	public static void main(String[] args) 
 	{ 
 		RXTXRobot r = new ArduinoNano(); // Create RXTXRobot object 
-		r.setPort("/dev/tty.wch ch341 USB=>RS232 1410"); // Set port to COM2 
+		r.setPort("/dev/tty.wch ch341 USB=>RS232 410"); // Set port to COM2 
 		r.connect(); 
 		//We don't have to attach anything because these motors are 
 		//attached by default 
-<<<<<<< Updated upstream
-		r.runEncodedMotor(RXTXRobot.MOTOR1, 255, 1000, RXTXRobot.MOTOR2, -255, 1000); //speed, ticks
-=======
-		r.runEncodedMotor(RXTXRobot.MOTOR1, 200, 250, RXTXRobot.MOTOR2, 200, 250); 
->>>>>>> Stashed changes
+
+		r.runEncodedMotor(RXTXRobot.MOTOR1, 200, 1200, RXTXRobot.MOTOR2, -220, 1200); //speed, ticks
+
+		//r.runEncodedMotor(RXTXRobot.MOTOR1, 200, 250, RXTXRobot.MOTOR2, 200, 250); 
+
 		r.close(); 
 	} 
 	
--- FirstYearDesign/src/RunTime.java
+++ FirstYearDesign/src/RunTime.java
@@ -8,64 +8,81 @@ public class RunTime
 	public static void main(String[] args){
 
 		Mech mechRobot = new Mech(); //creates a new Mechanical robot system
-		Env envRobot = new Env(); // creates a new Environmental robot system		
+		//Env envRobot = new Env(); // creates a new Environmental robot system		
 		
 		//set servo to angle 0
 		mechRobot.setServo();
+		mechRobot.setServo2();
 		
-		mechRobot.findBridge(0, 150, 65);
-		//find water
-		//use ping sensor to locate the distance to the wall, needs to be 12.7 centimeters (5 inches) from the wall
-		/*mechRobot.moveForward(0, 180, 38); //sends time, speed, and distance buffer, 0 denotes infinite time
-		
+		mechRobot.moveForward(0, 180, 34); //sends time, speed, and distance buffer, 0 denotes infinite time
+		mechRobot.sleep(1500);
+		mechRobot.stopMotors();
 		//test water
 		//test water for salinity and turbidity
+		
 		//envRobot.checkSalinity();
 		//envRobot.checkTurbidity();
 		
 		//locate first station
-		mechRobot.moveBackward(0, 180, 23);
+		mechRobot.moveBackward(0, 180, 20);
 		System.out.println("turn right");
 		mechRobot.turnRight();
 		System.out.println("move forward");
-		mechRobot.moveForward(0, 180, 48);
 		
-		//swing arm to dispense ping pong ball
-		//mechRobot.turbidityMaterials(210);
+		mechRobot.moveForward(0, 180, 43);
+		mechRobot.sleep(1500);
+		mechRobot.stopMotors();
+		
+		mechRobot.dispenseTenBalls(10);
 	
 		//locate second station
 		System.out.println("move backward");
 		mechRobot.moveBackward(0, 180, 40);
 		mechRobot.turnLeft();
-		mechRobot.moveBackward(0, 180, 68);
+		mechRobot.moveBackward(0, 180, 70);
 		mechRobot.turnRight();
-		mechRobot.moveForward(0, 180, 48);	
+		mechRobot.moveForward(0, 180, 49);
+		mechRobot.sleep(1500);
+		mechRobot.stopMotors();
+		mechRobot.dispenseTenBalls(10);
 		
 		//locate third station
 		mechRobot.moveBackward(0, 180, 35);
 		mechRobot.turnLeft();
 		mechRobot.turnLeft();
-		mechRobot.bothEncodedMotors(180);
 		mechRobot.moveForward(0, 180, 45);
+		mechRobot.sleep(1500);
+		mechRobot.stopMotors();
+		mechRobot.dispenseTenBalls(10);
+		
+		//locate fourth station
+		mechRobot.moveBackward(0, 180, 35);
+		mechRobot.turnRight();
+		mechRobot.moveForward(0, 180, 60);
+		mechRobot.turnLeft();
+		mechRobot.moveForward(0, 180, 45);
+		mechRobot.sleep(1500);
+		mechRobot.stopMotors();
+		mechRobot.dispenseTenBalls(10);
+		
+		//locate bridge
+		mechRobot.moveBackward(0, 180, 40);
+		mechRobot.turnRight();
+		mechRobot.moveBackward(0, 180, 60);
+		mechRobot.turnLeft();
+		mechRobot.sleep(1500);
+		mechRobot.stopMotors();
+		mechRobot.findBridge(0, 150, 65);
 
-		
-			
-		
-		
-		//swing claw to knock out ping pong balls into bucket
-		//use if statements to choose between using dispenseEven or dispenseOdd functions
-		*/
-		
-		//mechRobot.turn180();
-		
 		//System.out.println(envRobot.getConductivity()); //SALINITY 
 		
 		//System.out.println(envRobot.testWaterTurbidity()); //TURBIDITY
 		
 		mechRobot.close(); //closing connection
-		envRobot.close(); //closing connection
-		
+		//envRobot.close(); //closing connection
+
 	}
-}	
 	
+}	
+
 
